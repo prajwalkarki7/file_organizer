200163a97153ec12643823bc6ebb6bed20341383
Prajwal <karkiprajwal873@gmail.com>
2026-05-04 13:55:25 +0545

    enabling web

--- main.py
+++ main.py
@@ -3,16 +3,20 @@ from dotenv import load_dotenv
 load_dotenv()
 
 from agent import build_agent
+from pydantic_ai import Agent
 
-def main()->None:
-    agent=build_agent()
-    while True:
-        user_input=input("Enter your command (or 'exit' to quit): ")
-        if user_input=='exit':
-            print("Goodbye!")
-            break
-        response=agent.run_sync(user_input)
-        print(f"Agent: {response.output}")
+agent: Agent = build_agent()
+app = agent.to_web()
 
-if __name__ == "__main__":
-    main()
+# def main()->None:
+#     agent=build_agent()
+#     while True:
+#         user_input=input("Enter your command (or 'exit' to quit): ")
+#         if user_input=='exit':
+#             print("Goodbye!")
+#             break
+#         response=agent.run_sync(user_input)
+#         print(f"Agent: {response.output}")
+
+# if __name__ == "__main__":
+#     main()
